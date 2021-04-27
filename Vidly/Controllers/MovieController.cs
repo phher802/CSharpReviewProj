1c89963fc2298e705ff882f6a82ade1120d2d2c7
phher802 <pher024@gmail.com>
2021-04-26 23:50:38 -0500

    Add Movie folder in views and Random view

--- Vidly/Controllers/MovieController.cs
+++ Vidly/Controllers/MovieController.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class MovieController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Random()
         {
+            Movie movie = new Movie() { Name = "Shrek!" };
             return View();
         }
     }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -7,5 +7,10 @@ namespace Vidly.Models
 {
     public class Movie
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
     }
+
+
 }
