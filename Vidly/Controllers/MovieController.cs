8a87c19eebf6aa54d3cac85f77c08a7dab11a42b
phher802 <pher024@gmail.com>
2021-04-27 20:57:44 -0500

    Add Random view to Movie Views folder

--- Vidly/Controllers/MovieController.cs
+++ Vidly/Controllers/MovieController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class MovieController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Random()
         {
             Movie movie = new Movie() { Name = "Shrek!", Genre = "Animation", Director = "Andrew Adamson" };
             return View();
