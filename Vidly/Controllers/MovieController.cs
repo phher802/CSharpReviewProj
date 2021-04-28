ade766126649180cbe6dd3a864003dee4ddd525b
phher802 <pher024@gmail.com>
2021-04-27 20:54:44 -0500

    Create Movie class and MovieController

---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            Movie movie = new Movie() { Name = "Shrek!", Genre = "Animation", Director = "Andrew Adamson" };
+            return View();
+        }
+    }
+}
